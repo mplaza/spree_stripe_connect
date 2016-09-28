2f812079e0a4e42b73763884d3a93166123417a9
mplaza <melanieplaza@gmail.com>
2016-09-27 18:38:31 -0700

    skip before filter for redirect

--- app/controllers/spree/admin/stripe_redirect_controller.rb
+++ app/controllers/spree/admin/stripe_redirect_controller.rb
@@ -1,4 +1,5 @@
 class Spree::Admin::StripeRedirectController < ApplicationController
+	skip_before_filter :single_signon
 	def setup
 		code = params["code"]
 		brand_domain = Spree::Tenant.find_by_id(params["state"]).domain
